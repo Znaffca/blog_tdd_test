8aed72c4394b488280e0fdfbdb8483cdd5a26fcc
Grzegorz Salacinski <salacinski.grzegorz@gmail.com>
2019-06-14 11:02:11 +0200

    create simple blog model with few test checks __str__ and name

--- tdd_blog/blog/models.py
+++ tdd_blog/blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Entry(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True, editable=False)
+    modified = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Entries"
